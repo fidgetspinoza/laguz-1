ea8ffa6f899bf582c12713084142d98802bac9c6
Yura <yura@tbqh.net>
2022-12-02 15:39:15 +0000

    😼

--- Justfile
+++ Justfile
@@ -12,3 +12,5 @@ generate_flex project_name minter_addr bid_valid_denom symbol_721:
 generate_sample_flex:
   @just generate_flex "laguz-sample-1" "zzzzzandr15zxx4vntdvpzq3xlajufthgjp27qtzqwqa4umn" "uandrrrr" "LAGUZS"
 
+frontend_dev:
+  cd laguz-1-frontend && npm run dev
--- laguz-1-frontend/src/App.tsx
+++ laguz-1-frontend/src/App.tsx
@@ -1,27 +1,22 @@
 import AndromedaClient, * as andr from "@andromedaprotocol/andromeda.js";
 
-
-const madeAddressList = {
-  "$type": "address-list",
-  "$class": "module",
-  "$classifier": "address",
-  "$enabled": true,
-  "$removable": true,
-  "is_inclusive": true
-}
 import { Buffer } from "buffer"
 window.Buffer = window.Buffer || Buffer;
 
+const connectionConfig = {
+  endpoint: "https://rpc-andromeda-testnet.cereslabs.io/",
+  registryAddr: "andr14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9shptkql"
+}
+
+const appConfig = {
+  contractAddr: "andr1y6n8r02ch0vpt4xchxdt54gs56wplwwt7lm67ftals9q3v8q8ddqf32fty"
+}
+
 const a = new AndromedaClient()
-a.connect("https://rpc-andromeda-testnet.cereslabs.io/", "andr14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9shptkql").then(async thing => {
+a.connect(connectionConfig.endpoint, connectionConfig.registryAddr).then(async thing => {
   AndromedaClient
-  console.log(a.isConnected)
-  // const z = await andr.queryTxByAccount("galileo-2", "andr15zxx4vntdvpzq3xlajufthgjp27qtzqwqa4umn", )
-  const app = await andr.queryApp("andr1y6n8r02ch0vpt4xchxdt54gs56wplwwt7lm67ftals9q3v8q8ddqf32fty")
+  const app = await andr.queryApp(appConfig.contractAddr)
   console.log({ app })
-  const z = await andr.queryAddressListIncludesAddress("andr1h6jj96e5m07c4nz22jlu4tasegp65f64rqk6f4tzf89ppjk98d8qulya72", "andr15zxx4vntdvpzq3xlajufthgjp27qtzqwqa4umn")
-  console.log({ z })
-  // console.log(z.map(zz => JSON.parse(zz.rawLog)))
 })
 
 
